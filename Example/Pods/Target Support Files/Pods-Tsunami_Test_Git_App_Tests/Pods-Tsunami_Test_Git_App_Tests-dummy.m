4868f1e06f92e260e78905b38f6e187573ab3925
TsunamiLi <769681799@qq.com>
2022-04-19 14:31:11 +0800

    feat:新增文件

---
+++ Example/Pods/Target Support Files/Pods-Tsunami_Test_Git_App_Tests/Pods-Tsunami_Test_Git_App_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tsunami_Test_Git_App_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Tsunami_Test_Git_App_Tests
+@end
---
+++ Example/Pods/Target Support Files/Tsunami_Test_Git_App/Tsunami_Test_Git_App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Tsunami_Test_Git_App : NSObject
+@end
+@implementation PodsDummy_Tsunami_Test_Git_App
+@end
